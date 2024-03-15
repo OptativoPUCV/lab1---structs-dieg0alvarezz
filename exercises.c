8a6f2f26db48571e0079b8e991919c4ffcc6b91f
Your Name <you@example.com>
2024-03-15 22:34:20 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -50,28 +50,26 @@ newsize apunta a una dirección válida que no ha sido inicializada con nigún v
 
 */
 
-
-
 int *filterEvenNumbers(int arr[], int size, int *newSize) {
-    int *newArr = NULL;
-    *newSize = 0;
 
-    for (int i = 0; i < size; i++) {
-        if (arr[i] % 2 == 0) {
-          
-            // Reasignamos newArr con el tamaño actualizado
-            newArr = realloc(newArr, (*newSize + 1) * sizeof(int));
-            newArr[*newSize] = arr[i];
-            (*newSize)++; // Incrementamos el tamaño del nuevo arreglo
-        }
-      
+  int *newArr = NULL;
+ 
+  //int posActual = 0;
+  
+  for (int i = 0; i < size; i++)
+  {
+    if (arr[i] % 2 == 0)
+    {
+      newArr = realloc(newArr, (*newSize + 1) * sizeof(int));
+      newArr[*newSize] = arr[i];
+      *newSize +=1;
     }
 
-
+  }
+  return newArr;
   
-    return newArr;
-}
-
+  
+  }
 
 
 
